0d81f2bd8c0404c12bd8f5fdda9df2e30b49878a
DmitriySe666 <semakindi@gmail.com>
2024-10-12 19:15:54 +0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+print ('1st program')
+print (9**0.5 * 5)
+
+print ('2nd program')
+print (9.999 > 9.998 and 1000 != 1000.1)
+
+print ('3rd program')
+print (1234 % 1000 // 10 + 5678 % 1000 //10)
+
+print ('4th program')
+print ( int (13.42) == 42.13 // 0.01 % 100) #float дробь int целое 13.42 и 42.13
